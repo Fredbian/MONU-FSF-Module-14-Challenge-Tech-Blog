9ebca6797b8da3abfb4ee6e66b9f3c35f4178d8f
Tao Bian <biantao0519@hotmail.com>
2022-08-09 14:45:28 +1000

    set up structure

---
+++ server.js
@@ -0,0 +1,43 @@
+// load modules
+const express = require('express')
+const path = require('path')
+const session = require('express-session')
+const routes = require('./routes/index')
+const exphbs = require('express-handlebars')
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create({ helpers })
+// import sequelize connection
+const sequelize = require('./db/connection')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+
+// set up express PORT
+const app = express()
+const PORT = process.env.PORT || 3001
+
+// create sequelize store
+const sess = {
+    secret: 'SECRET',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+
+// session middleware
+app.use(session(sess))
+
+// set handlebars engine
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+app.use(express.json())
+app.use(routes)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log("Now listening http://localhost:" + PORT))
+})
---
+++ utils/helpers.js
@@ -0,0 +1,6 @@
+const formatDate = (date) => {
+    return `${new Date(date).getMonth() + 1}/$${new Date(date).getDate()}/${new Date(
+        date).getFullYear()}`
+}
+
+module.exports = formatDate
